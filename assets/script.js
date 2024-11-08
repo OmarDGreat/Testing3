96ee99855d310f7c1212309b928a9887ded405cd
Gurprasad Singh <gurprasadsingh@gurprasads-mbp.mynetworksettings.com>
2024-11-07 20:21:28 -0500

    Updates for adding amounts

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,26 @@
 const amountInput = document.getElementById("amountInput")
 const amountDisplay = document.getElementById("amountDisplay")
 const submitButton = document.getElementById("submitButton")
+const limitButton = document.getElementById("limitButton")
 
 function displayAmount(){
     const enteredAmount = amountInput.value;
-    amountDisplay.textContent = "Amount : $" + enteredAmount;
+    console.log(enteredAmount);
+    const currentTotalAmount = amountDisplay.textContent
+    const newTotalAmount = parseInt(currentTotalAmount) + parseInt(enteredAmount)
+    console.log(newTotalAmount);
+    amountDisplay.textContent = newTotalAmount;
+}
+
+function limitPrompt(){
+    let userInput = prompt("Enter budget limit")
+
+
 }
 
 submitButton.addEventListener('click',displayAmount);
+limitButton.addEventListener('click',limitPrompt);
+
 
 
 console.log("test");
